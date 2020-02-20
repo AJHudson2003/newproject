8cafc04aa69c65a9f92f2801c6f3bf78a561a7c4
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2020-02-20 09:29:18 -0500

    added button to move to next page and new activity main
    tested on nexus 5

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-        import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gotogame (View view){
+        Intent intent = new Intent(this, gameactivity.class);
+        startActivity(intent);
+    }
+
 }
