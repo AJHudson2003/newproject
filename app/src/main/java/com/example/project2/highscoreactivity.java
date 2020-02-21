f45c42a7d77d6b0785a881f2c2e4a92b243a50be
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2020-02-21 08:56:12 -0500

    added a button and also made it go to the main page
    tested on nexus 5

--- app/src/main/java/com/example/project2/highscoreactivity.java
+++ app/src/main/java/com/example/project2/highscoreactivity.java
@@ -2,7 +2,9 @@ package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class highscoreactivity extends AppCompatActivity {
 
@@ -11,4 +13,8 @@ public class highscoreactivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscoreactivity);
     }
-}
+    public void gotomain (View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}  
