f5470821ebed6b61157939261b2798c2d1885f92
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2020-02-21 09:11:18 -0500

    added new ships to game
    tested on nexus 5

--- app/src/main/java/com/example/project2/gameactivity.java
+++ app/src/main/java/com/example/project2/gameactivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.MotionEvent;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.annotation.SuppressLint;
 
 public class gameactivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gameactivity);
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_gameactivity);
     }
     public void gotomain (View view){
         Intent intent = new Intent(this, MainActivity.class);
