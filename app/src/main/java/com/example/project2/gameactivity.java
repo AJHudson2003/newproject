f83cc614fa83ca3197a238837631e11ef2f931c3
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2020-03-05 08:56:20 -0500

    fixed spaces for game
    tested on nexus 5

--- app/src/main/java/com/example/project2/gameactivity.java
+++ app/src/main/java/com/example/project2/gameactivity.java
@@ -5,13 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.MotionEvent;
-import android.view.View.OnTouchListener;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
-import android.annotation.SuppressLint;
+
 
 public class gameactivity extends AppCompatActivity {
 
